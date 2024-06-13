647b831ccebaf720fbb3fb7175a9315c82251e65
Süleyman Samet Kaya <69515911+suleymansametkaya@users.noreply.github.com>
2024-06-13 16:49:32 +0300

    left drone code

---
+++ sol drone.py
@@ -0,0 +1,161 @@
+# Description: IHA'nin belirtilen irtifaya yukselmesi ve belirtilen alanin taranmasi
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
+import time
+from pymavlink import mavutil
+
+# Drone Baglantisi icin ip ve port secimi
+dronebaglanti = "127.00.1:14560"
+
+# Drone baglantisi yapiliyor
+soldrone = connect(f'{dronebaglanti}', wait_ready=True)
+
+# IHA'nin arm edilmesi ve belirtilen irtifaya yukselmesi
+def takeoff(irtifa):
+
+    # IHA'nin arm edilebilir duruma gelesiye kadar bekleniyor 
+    while soldrone.is_armable is not True:
+        print("İHA arm edilebilir durumda değil.")
+        time.sleep(1)
+
+    print("İHA arm edilebilir.")
+
+    # IHA GUIDED moduna aliniyor
+    soldrone.mode = VehicleMode("GUIDED")
+
+    # IHA arm ediliyor
+    soldrone.armed = True
+
+    # IHA arm edilene kadar bekleniyor
+    while soldrone.armed is not True:
+        print("İHA arm ediliyor...")
+        time.sleep(0.5)
+
+    print("İHA arm edildi.")
+
+    # IHA belirtilen irtifaya yukseltiliyor
+    soldrone.simple_takeoff(irtifa)
+    
+    # IHA belirtilen irtifaya yukselene kadar bekleniyor
+    while soldrone.location.global_relative_frame.alt < irtifa * 0.9:
+        print("İha hedefe yükseliyor.")
+        time.sleep(1)
+
+# takeoff(7)
+
+# IHA'nin hiz ve konum bilgileri
+soldrone.airspeed = 2
+soldrone.groundspeed = 2
+soldrone.velocity[0] =2
+soldrone._yawspeed=2
+soldrone._pitchspeed=2
+
+
+# -35.3632019, 149.1649845, -35.3631713, 149.1659769, -35.3621192, 149.1658482, -35.3621257, 149.1648799
+#       X1,         Y1,          X2,         Y2,             X3,         Y3,            X4,      Y4
+
+
+# Alanin taranmasi icin belirlenen noktalarin koordinatlarinin alinmasi ve IHA'nin bu noktalari tarayacak sekilde hareket etmesi
+def tara(x1,y1,x2,y2,x3,y3,x4,y4):
+
+    x_orta1 = (x1+x4)/2
+    x_orta2 = (x1+x_orta1)/2
+    x_orta3 = (x4+x_orta1)/2
+    y_orta1 = (y1+y2)/2
+    
+    global komut
+    komut = soldrone.commands
+    komut.clear()
+    time.sleep(1)
+    
+    
+    #takeoff
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 7))#cmd0
+    
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0, 0,0, x1, y1, 7))#cmd1
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_DELAY, 0, 0, 1, 0, 0, 0, 0, 0, 0))#cmd2
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0, 0,0,x1, y_orta1, 7))#cmd3
+
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta2, y_orta1, 7))#cmd4
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_DELAY, 0, 0, 1, 0, 0, 0, 0, 0, 0))#cmd5
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta2, y1, 7))#cmd6
+
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta1, y1, 7))#cmd7
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_DELAY, 0, 0, 1, 0, 0, 0, 0, 0, 0))#cmd8
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta1, y_orta1, 7))#cmd9
+
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta3, y_orta1, 7))#cmd10
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_DELAY, 0, 0, 1, 0, 0, 0, 0, 0, 0))#cmd11
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x_orta3, y1, 7))#cmd12
+
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x4, y4, 7))#cmd13
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_DELAY, 0, 0, 1, 0, 0, 0, 0, 0, 0))#cmd14
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 1, 0,0,0, x4, y_orta1, 7))#cmd15
+
+    #rtl ---> iha rtl yaptığında irtifa 15 metre ye çıkartıyo 
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0, 0))#cmd16
+    #gorev bitiş doğrulama
+    komut.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0, 0))#cmd17
+    
+    komut.upload()
+    print("Komutlar yukleniyor.\n")
+    
+    
+takeoff(7)    
+# -35,3631429 ,149,1645849, -35,3631429 ,149,1656604, -35,3621104, 149,1656604,  -35,3621104, 149,1645849
+# x1, x4,
+
+# -35.3632019, 149.1649845, -35.3631713, 149.1659769, -35.3621192, 149.1658482, -35.3621257, 149.1648799
+#       X1,         Y1,          X2,         Y2,             X3,         Y3,            X4,      Y4
+
+
+
+tara(-35.3632019, 149.1649845, -35.3631713, 149.1659769, -35.3621192, 149.1658482, -35.3621257, 149.1648799)
+    
+   
+komut.next = 0
+# IHA AUTO moduna aliniyor
+soldrone.mode = VehicleMode("AUTO")
+time.sleep(1)
+print(f"İha : " + str(soldrone.mode) + " moduna alindi.\n")
+
+# IHA'nin belirtilen komutlari gerceklestirmesi
+while True:
+    
+    soldrone.airspeed = 2
+    soldrone.groundspeed = 2
+    soldrone.velocity[0] =2
+
+    next_komut = komut.next
+    print(f"Siradaki komut : {next_komut}\n")
+    time.sleep(2.5)
+    print(f"Konum X : " + str(soldrone.location.global_relative_frame.lat))
+    print(f"Konum Y : " + str(soldrone.location.global_relative_frame.lon))
+    print(f"Konum Z : " + str(soldrone.location.global_relative_frame.alt))
+    print("air speed : " + str(soldrone.airspeed))
+    print("ground speed : " + str(soldrone.groundspeed))
+    print("----------------------------")
+   
+    if next_komut is 10:
+        print("Alanin yarisi tarandi\n")
+        print("Alanin yarisi tarandi\n")
+
+
+    time.sleep(1)
+    if next_komut is 13:
+        print(" Alan Tarama Gorevi Bitti\n")
+        print(" Alan Tarama Gorevi Bitti\n")
+        print(" Alan Tarama Gorevi Bitti\n")
+        
+
+    if next_komut is 16:
+
+        print("RTL yapiliyor...\n")
+        time.sleep(2.5)
+
+    
+    if next_komut is 17:
+
+        print("RTL Bitti\n")
+        time.sleep(1)
+        break
